c2a81f16607f9ff8c576e28dadf5e97d71956bcc
pinstripeninjas <pinstripeninjas@gmail.com>
2020-06-09 12:48:20 -0700

    removed unecessary js

--- static/js/dropdown.js
+++
@@ -1,9 +0,0 @@
-const dropdown = document.querySelector(".dropdown");
-const links = document.querySelector("#links");
-
-links.addEventListener("mouseenter", showDropdown);
-
-function showDropdown() {
-	dropdown.classList.toggle("display-none");
-	console.log("clicked");
-}
